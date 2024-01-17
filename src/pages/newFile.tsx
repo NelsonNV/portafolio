9bb6f5714e8b86e1b34cbf182d6bda32170d6271
NelsonNV <nelsonluisnv2@gmail.com>
2024-01-17 20:03:48 -0300

    ajustes visuales y nuevos item

---
+++ src/pages/newFile.tsx
@@ -0,0 +1,11 @@
+import Layout from '../layouts/Layout.astro';
+
+<Fragment>
+<Layout title="Welcome my portafolio">
+<main>
+<h1> Hey, Soy Nelson Navarro <Badge> Disponible para trbajar</Badge> </h1>
+<h2>3 años de Estudio, Desarrollador web, Soy de chile. Me especializo en back End</h2>
+</main>
+</Layout>
+
+</Fragment>;
